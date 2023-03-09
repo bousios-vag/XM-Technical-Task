7c9e9b40c8ecfe405577db03e0fd96e45bafd8fc
Vaggelis Bousios <vbousios@plaisio.gr>
2023-03-09 22:41:19 +0200

    final

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const { src, series, parallel, dest, watch} = require('gulp');
 const cssPath = 'assets/css/**/*.css';
 
 
+const jsPath = 'assets/js/**/*.js';
+
+
 function copyHtml() {
   return src('src/*.html').pipe(gulp.dest('dist'));
 }
@@ -17,11 +20,17 @@ function cssTask() {
     .pipe(dest('dist/xmtechnicaltask-assets/css'));
 }
 
+function jsTask() {
+  return src(jsPath)
+    .pipe(dest('dist/xmtechnicaltask-assets/js'));
+}
+
 
 
 exports.cssTask = cssTask;
 exports.imgTask = imgTask;
+exports.jsTask = jsTask;
 exports.copyHtml = copyHtml;
 exports.vangelas = series(
-  parallel(copyHtml, imgTask, cssTask)
+  parallel(copyHtml, imgTask, cssTask,jsTask)
 );
